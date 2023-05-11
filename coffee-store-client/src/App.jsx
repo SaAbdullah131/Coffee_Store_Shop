58804e6c69a7ec9a7d04ae1f25d9f5bc9feeb2f1
SaAbdullah131 <cse.asabdullah@gmail.com>
2023-05-11 18:42:01 +0600

    coffee store client and server steup

---
+++ coffee-store-client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <h1 className='text-6xl text-violet-600'>Coffee Store shop</h1>
+     
+    </>
+  )
+}
+
+export default App
